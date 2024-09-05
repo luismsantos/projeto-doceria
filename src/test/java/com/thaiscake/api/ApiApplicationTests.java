f560276d1e273325b0184434862deef19f311438
Luis Miguel <luismiguelsantossouza@gmail.com>
2024-09-05 19:11:57 -0300

    first commit

---
+++ src/test/java/com/thaiscake/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaiscake.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
