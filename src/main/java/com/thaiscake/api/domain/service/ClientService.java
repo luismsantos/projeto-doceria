62a532582f984ca44304619894942589edf32b94
luis <luismiguelsantossouza@gmail.com>
2024-09-10 11:48:16 -0300

    feat: edit ClientService

--- src/main/java/com/thaiscake/api/domain/service/ClientService.java
+++ src/main/java/com/thaiscake/api/domain/service/ClientService.java
@@ -1,7 +1,19 @@
 package com.thaiscake.api.domain.service;
 
+import com.thaiscake.api.domain.client.Client;
+import com.thaiscake.api.domain.client.ClientRequestDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
+    public Client createClient(ClientRequestDTO data){
+    Client newClient = new Client();
+    newClient.setName(data.name());
+    newClient.setAddress(data.address());
+    newClient.setCpf(data.cpf());
+    newClient.setAge(data.age());
+
+    return newClient;
+    }
+
 }
