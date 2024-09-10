cd767698a05496788f6c2ad0f74360ce96b9d7ca
luis <luismiguelsantossouza@gmail.com>
2024-09-10 11:49:13 -0300

    feat: edit OrderRepository

--- src/main/java/com/thaiscake/api/repositories/OrderRepository.java
+++ src/main/java/com/thaiscake/api/repositories/OrderRepository.java
@@ -1,6 +1,6 @@
 package com.thaiscake.api.repositories;
 
-import com.thaiscake.api.domain.Order;
+import com.thaiscake.api.domain.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
